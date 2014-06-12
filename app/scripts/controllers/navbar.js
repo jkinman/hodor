fdb77966e60756324ff9105c8d5353a1993aed12
Joel Kinman <joel.kinman@gmail.com>
2014-06-11 23:08:37 -0700

    reformatting

--- app/scripts/controllers/navbar.js
+++ app/scripts/controllers/navbar.js
@@ -1,29 +1,26 @@
 'use strict';
 
 angular.module('hodorApp')
-  .controller('NavbarCtrl', function ($scope, $location, Auth) {
-    $scope.menu = [{
-      'title': 'Home',
-      'link': '/'
-    },
-    {
-      'title': 'Account',
-      'link': '/settings'
-    },
-    {
-      'title': 'Shirts',
-      'link': '/clothes'
-    }
-    ];
-    
-    $scope.logout = function() {
-      Auth.logout()
-      .then(function() {
-        $location.path('/login');
-      });
-    };
-    
-    $scope.isActive = function(route) {
-      return route === $location.path();
-    };
-  });
+	.controller('NavbarCtrl', function($scope, $location, Auth) {
+		$scope.menu = [{
+			'title': 'Home',
+			'link': '/'
+		}, {
+			'title': 'Account',
+			'link': '/settings'
+		}, {
+			'title': 'Shirts',
+			'link': '/clothes'
+		}];
+
+		$scope.logout = function() {
+			Auth.logout()
+				.then(function() {
+					$location.path('/login');
+				});
+		};
+
+		$scope.isActive = function(route) {
+			return route === $location.path();
+		};
+	});
