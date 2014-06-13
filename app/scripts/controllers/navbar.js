5ee0c27aff7b728b85784915809c4941573a55c8
Joel Kinman <joel.kinman@gmail.com>
2014-06-13 14:02:54 -0700

    navbar update added hodorize functions

--- app/scripts/controllers/navbar.js
+++ app/scripts/controllers/navbar.js
@@ -5,19 +5,35 @@ angular.module('hodorApp')
 		$scope.menu = [{
 			'title': 'Home',
 			'link': '/'
-		}, {
-			'title': 'Account',
-			'link': '/settings'
-		}, {
+    // }, {
+    //  'title': 'Account',
+    //  'link': '/settings'
+  }, {
 			'title': 'Shirts',
 			'link': '/clothes'
 		}];
-
+		var lastHodor = '';
 		$scope.logout = function() {
 			Auth.logout()
 				.then(function() {
 					$location.path('/login');
 				});
+		};
+
+		$scope.hodorize = function() {
+			// debugger;
+			lastHodor = this.item.title;
+			this.item.title = 'HODOR';
+			//$(e).innerHTML = 'HODOR';
+
+		};
+
+		$scope.unhodorize = function() {
+			// debugger;
+			// lastHodor = this.item.title;
+			this.item.title = lastHodor;
+			//$(e).innerHTML = 'HODOR';
+
 		};
 
 		$scope.isActive = function(route) {
